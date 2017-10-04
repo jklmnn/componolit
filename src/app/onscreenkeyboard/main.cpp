f6f67991a91cd0160327fa3ab4ba6341219ab1d7
Johannes Kliemann <kliemann@componolit.com>
2017-10-04 14:10:10 +0200

    Changed main to libc component.

--- src/app/onscreenkeyboard/main.cpp
+++ src/app/onscreenkeyboard/main.cpp
@@ -1,19 +1,31 @@
 
+#include <libc/component.h>
+
 #include <QQuickView>
 #include <QGuiApplication>
 #include <QQmlContext>
 
 #include "oskinputmethod.h"
 
-int main(int argc, char *argv[])
+extern void initialize_qt_core(Genode::Env &);
+extern void initialize_qt_gui(Genode::Env &);
+
+void Libc::Component::construct(Libc::Env &env)
 {
-    qputenv("QT_IM_MODULE", "qtvirtualkeyboard");
-    OskInputMethod *oim = new OskInputMethod();
-    QGuiApplication app(argc, argv);
-    QQuickView view(QString("qrc:/main.qml"));
-    QQmlContext *context = view.rootContext();
-    context->setContextProperty(QStringLiteral("OskInputMethod"), oim);
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.show();
-    return app.exec();
+
+    Libc::with_libc([&]{
+            initialize_qt_core(env);
+            initialize_qt_gui(env);
+            int argc = 1;
+            char *argv[] = {"osk"};
+            qputenv("QT_IM_MODULE", "qtvirtualkeyboard");
+            OskInputMethod *oim = new OskInputMethod();
+            QGuiApplication app(argc, argv);
+            QQuickView view(QString("qrc:/main.qml"));
+            QQmlContext *context = view.rootContext();
+            context->setContextProperty(QStringLiteral("OskInputMethod"), oim);
+            view.setResizeMode(QQuickView::SizeRootObjectToView);
+            view.show();
+            return app.exec();
+    });
 }
--- src/app/onscreenkeyboard/target.mk
+++ src/app/onscreenkeyboard/target.mk
@@ -8,6 +8,6 @@ include $(QT5_REP_DIR)/src/app/qt5/tmpl/target_defaults.inc
 
 include $(QT5_REP_DIR)/src/app/qt5/tmpl/target_final.inc
 
-LIBS 	+= qoost qt5_component qt5_qtvirtualkeyboardplugin
+LIBS 	+= qoost qt5_qtvirtualkeyboardplugin
 #INC_DIR	+= $(QT5_CONTRIB_DIR)/qtvirtualkeyboard/src/virtualkeyboard\
 	   $(QT5_CONTRIB_DIR)/qtbase/include/QtCore/5.8.0
