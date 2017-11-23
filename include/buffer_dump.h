3dc8e3a3c99f27fb0d22fe7d9b74bd91c4e27a48
Johannes Kliemann <kliemann@componolit.com>
2017-11-23 14:23:48 +0100

    add license header

--- include/buffer_dump.h
+++ include/buffer_dump.h
@@ -1,3 +1,16 @@
+/*
+ * \brief Helper class to log arbitrary memory ojects
+ * \author Johannes Kliemann
+ * \date 2017-11-23
+ */
+
+/*
+ * Copyright (C) 2017 Componolit GmbH
+ *
+ * This file is part of the Componolit platform, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
 
 #ifndef _BUFFER_DUMP_H_
 #define _BUFFER_DUMP_H_
