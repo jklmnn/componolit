5e4c7108c41e79dfc0e3abb68fe25ff7c40d4f32
Johannes Kliemann <kliemann@componolit.com>
2018-05-31 14:54:15 +0200

    working prototype

--- src/server/http_uplink/connection.cc
+++ src/server/http_uplink/connection.cc
@@ -3,40 +3,51 @@
 #include <unistd.h>
 
 Http_Filter::Connection::Connection(Genode::Env &env, int socket, Genode::String<32> label) :
-    Genode::Thread(env, label.string(), 4096),
+    Genode::Thread(env, label.string(), BUFSIZE + 4096),
+    _env(env),
     _terminal(env, label.string()),
     _read_sigh(env.ep(), *this, &Http_Filter::Connection::handle_response),
     _socket(socket)
 {
+    Genode::log(__func__);
     _terminal.read_avail_sigh(_read_sigh);
+    Genode::Signal_transmitter(_read_sigh).submit();
 }
 
 void Http_Filter::Connection::entry()
 {
+    Genode::log("entry submit");
+    Genode::Signal_handler<Connection> sigh(_env.ep(), *this, &Connection::handle_response);
+    Genode::Signal_transmitter(sigh).submit();
     char buffer[BUFSIZE];
     long size = 0;
 
     while (size = read(_socket, buffer, BUFSIZE), size >= 0){
+        Genode::log("read ", size);
         while(size > 0){
+            Genode::warning("write loop");
             size -= _terminal.write(buffer, size);
         }
     }
-
+    close(_socket);
+    Genode::log("Closing connection");
     //FIXME: notify parent thread that thread returned
 }
 
 void Http_Filter::Connection::handle_response()
 {
+    Genode::error(__func__);
     char buffer[BUFSIZE];
     Genode::size_t size = _terminal.read(buffer, BUFSIZE);
     long ssize;
 
     while(size > 0){
+        Genode::log("write ", size);
         ssize = write(_socket, buffer, size);
         if(ssize < 0){
             Genode::error("Failed to write: ", ssize);
             return;
         }
-        size += ssize;
+        size -= ssize;
     }
 }
--- src/server/http_uplink/main.cc
+++ src/server/http_uplink/main.cc
@@ -1,91 +1,53 @@
 
 #include <libc/component.h>
 #include <timer_session/connection.h>
+#include <base/signal.h>
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-
-#include <connection.h>
+#include <server.h>
 
 namespace Http_Filter
 {
     struct Main;
-    enum {
-        CONNECTION_COUNT = 10
-    };
 }
 
 struct Http_Filter::Main
 {
     Genode::Env &_env;
     Timer::Connection _timer;
+    Genode::Signal_handler<Main> _csigh;
+    Server _server;
+    Genode::Constructible<Connection> _connection_pool[CONNECTION_COUNT];
+    Genode::String<32> _label;
+    int _connection;
 
-    Genode::Constructible<Connection> Connection_Pool[CONNECTION_COUNT];
-
-    void listen_and_dispatch()
+    void handle_connection()
     {
-        struct sockaddr_in serv, client;
-        socklen_t client_length;
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        int conn;
-
-        Genode::memset (&serv, 0, sizeof(struct sockaddr_in));
-
-        serv.sin_family = AF_INET;
-        serv.sin_addr.s_addr = INADDR_ANY;
-        serv.sin_port = htons(80);
-
-        if (bind(sock, (struct sockaddr *)&serv, sizeof(serv)) < 0){
-            Genode::error("Failed to bind");
-            return;
-        }
-
-        listen(sock, CONNECTION_COUNT);
-
-        while (true){
-            Genode::memset (&client, 0, sizeof(struct sockaddr_in));
-            client_length = sizeof(client);
-            conn = accept(sock, (struct sockaddr*)&client, &client_length);
-            if (conn >= 0){
-                bool pool_not_full = false;
-                char lbl[32] = {};
-
-                snprintf(lbl, 31, "%u.%u.%u.%u:%d",
-                        (unsigned)ntohl(client.sin_addr.s_addr) >> 24,
-                        ((unsigned)ntohl(client.sin_addr.s_addr) >> 16) & 0xff,
-                        ((unsigned)ntohl(client.sin_addr.s_addr) >> 8) & 0xff,
-                        (unsigned)ntohl(client.sin_addr.s_addr) & 0xff,
-                        ntohs(client.sin_port));
-                Genode::String<32> label = Genode::String<32>(lbl);
-                Genode::log("Connection from: ", label);
-
-                for(unsigned i = 0; i < sizeof(Connection_Pool) / sizeof(Connection); i++){
-                    if (!Connection_Pool[i].constructed()){
-                        pool_not_full = true;
-                        Connection_Pool[i].construct(_env, conn, label);
-                        Connection_Pool[i]->start();
-                    }
-                }
-                if(!pool_not_full){
-                    Genode::warning("Connection pool is full, closing connection");
-                    close(conn);
-                }
-            }else{
-                Genode::error("Connection failed");
+        bool pool_not_full = false;
+        Genode::log(__func__);
+        for(unsigned i = 0; i < sizeof(_connection_pool) / sizeof(Connection); i++){
+            if (!_connection_pool[i].constructed()){
+                pool_not_full = true;
+                _connection_pool[i].construct(_env, _connection, _label);
+                _connection_pool[i]->start();
             }
+        }
+        if(!pool_not_full){
+            Genode::warning("Connection pool is full, closing connection");
         }
     }
 
-    Main(Genode::Env &env) : _env(env), _timer(env)
+    Main(Genode::Env &env) :
+        _env(env),
+        _timer(env),
+        _csigh(env.ep(), *this, &Main::handle_connection),
+        _server(env, _connection, _label, _csigh),
+        _label(""),
+        _connection(-1)
     {
         Genode::log("http_uplink");
         _timer.msleep(6000);
         Libc::with_libc([&](){
-                listen_and_dispatch();
+                _server.start();
                 });
     }
 };
