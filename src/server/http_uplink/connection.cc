5b8adf366cd402ae2c887dcde3facc3d2b206c28
Johannes Kliemann <kliemann@componolit.com>
2018-06-04 11:11:33 +0200

    separate thread from connection object

--- src/server/http_uplink/connection.cc
+++ src/server/http_uplink/connection.cc
@@ -3,35 +3,14 @@
 #include <unistd.h>
 
 Http_Filter::Connection::Connection(Genode::Env &env, int socket, Genode::String<32> label) :
-    Genode::Thread(env, label.string(), BUFSIZE + 4096),
     _env(env),
     _terminal(env, label.string()),
     _read_sigh(env.ep(), *this, &Http_Filter::Connection::handle_response),
-    _socket(socket)
+    _socket(socket),
+    _loop(env, socket, label, _terminal)
 {
     Genode::log(__func__);
     _terminal.read_avail_sigh(_read_sigh);
-    Genode::Signal_transmitter(_read_sigh).submit();
-}
-
-void Http_Filter::Connection::entry()
-{
-    Genode::log("entry submit");
-    Genode::Signal_handler<Connection> sigh(_env.ep(), *this, &Connection::handle_response);
-    Genode::Signal_transmitter(sigh).submit();
-    char buffer[BUFSIZE];
-    long size = 0;
-
-    while (size = read(_socket, buffer, BUFSIZE), size >= 0){
-        Genode::log("read ", size);
-        while(size > 0){
-            Genode::warning("write loop");
-            size -= _terminal.write(buffer, size);
-        }
-    }
-    close(_socket);
-    Genode::log("Closing connection");
-    //FIXME: notify parent thread that thread returned
 }
 
 void Http_Filter::Connection::handle_response()
@@ -51,3 +30,34 @@ void Http_Filter::Connection::handle_response()
         size -= ssize;
     }
 }
+
+void Http_Filter::Connection::start()
+{
+    _loop.start();
+}
+
+Http_Filter::Connection_loop::Connection_loop(Genode::Env &env, int socket, Genode::String<32> label,
+        Terminal::Connection &terminal) :
+    Genode::Thread(env, label.string(), BUFSIZE + 4096),
+    _socket(socket),
+    _terminal(terminal)
+{ }
+
+void Http_Filter::Connection_loop::entry()
+{
+    Genode::log("entry submit");
+    char buffer[BUFSIZE];
+    long size;
+
+    while (size = read(_socket, buffer, BUFSIZE), size > 0){
+        Genode::log("read ", size);
+        while(size > 0){
+            Genode::warning("write loop");
+            size -= _terminal.write(buffer, size);
+        }
+    }
+    close(_socket);
+    Genode::log("Closing connection");
+    //FIXME: notify parent thread that thread returned
+}
+
