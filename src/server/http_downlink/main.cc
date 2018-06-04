f0af856ebe96bae779c86a445ef96570d1123241
Johannes Kliemann <kliemann@componolit.com>
2018-06-04 15:32:23 +0200

    get downlink information from config

--- src/server/http_downlink/main.cc
+++ src/server/http_downlink/main.cc
@@ -3,6 +3,7 @@
 #include <root/component.h>
 #include <base/heap.h>
 #include <timer_session/connection.h>
+#include <base/attached_rom_dataspace.h>
 
 #include <component.h>
 
@@ -15,10 +16,13 @@ struct Http_Filter::Main
 {
     Genode::Env &_env;
     Timer::Connection _timer;
+    Genode::Attached_rom_dataspace _config {_env, "config"};
 
     Genode::Sliced_heap _heap { _env.ram(), _env.rm() };
 
-    Root _root { _env, _env.ep(), _heap, _env.ram(), _env.rm() };
+    Root _root { _env, _env.ep(), _heap, _env.ram(), _env.rm(),
+        _config.xml().attribute_value<Genode::String<16>>("server_ip", "0.0.0.0"),
+        static_cast<int>(_config.xml().attribute_value<long int>("server_port", 0)) };
 
     Main(Genode::Env &env) : _env(env), _timer(env)
     {
