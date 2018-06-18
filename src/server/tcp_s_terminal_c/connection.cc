b31d28eb018e41d33981e921ea20ea4024abe893
Johannes Kliemann <kliemann@componolit.com>
2018-06-18 15:59:14 +0200

    terminal client interface

--- src/server/tcp_s_terminal_c/connection.cc
+++ src/server/tcp_s_terminal_c/connection.cc
@@ -1,19 +1,80 @@
 
 #include <connection.h>
 
+#include <unistd.h>
+
 Tcp::Connection::Connection(Genode::Env &env, int socket, char const *label) :
     Genode::Thread(env, label, CONNECTION_BUFFER + 4096),
+    _terminal(env, label),
+    _read_sigh(env.ep(), *this, &Tcp::Connection::handle_response),
     _socket(socket),
     _closed(false)
-{ }
+{
+    _terminal.read_avail_sigh(_read_sigh);
+}
 
 void Tcp::Connection::entry()
 {
-    Genode::log(__func__);
-    _closed = true;
+    char buffer[CONNECTION_BUFFER];
+    long received, sent;
+
+    while(!_closed){
+        Genode::memset(buffer, 0, sizeof(buffer));
+        sent = 0;
+        received = 0;
+        LIBC(read, buffer, sizeof(buffer), &received);
+        if(received > 0 && !_closed){
+            while(sent < received){
+                sent += _terminal.write(&buffer[sent], received - sent);
+                LIBC(close);
+            }
+        }
+    }
+}
+
+void Tcp::Connection::handle_response()
+{
+    char buffer[CONNECTION_BUFFER];
+    long received, sent;
+
+    if(!_terminal.avail()){
+        LIBC(close);
+        return;
+    }
+
+    while(!_closed && _terminal.avail()){
+        Genode::memset(buffer, 0, sizeof(buffer));
+        sent = 0;
+        received = _terminal.read(buffer, sizeof(buffer));
+        while(sent < received){
+            LIBC(write, &buffer[sent], received - sent, &sent);
+        }
+    }
 }
 
 bool Tcp::Connection::closed() const
 {
     return _closed;
+}
+
+void Tcp::Connection::lc_read(void *buffer, Genode::size_t size, long *result)
+{
+    *result += read(_socket, buffer, size);
+    if(*result < 1){
+        LIBC(close);
+    }
+}
+
+void Tcp::Connection::lc_write(void const *buffer, Genode::size_t size, long *result)
+{
+    *result += write(_socket, buffer, size);
+    if(*result < 1){
+        LIBC(close);
+    }
+}
+
+void Tcp::Connection::lc_close()
+{
+    _closed = true;
+    close(_socket);
 }
--- src/server/tcp_s_terminal_c/connection.h
+++ src/server/tcp_s_terminal_c/connection.h
@@ -2,6 +2,8 @@
 #include <libc_wrapper.h>
 
 #include <base/thread.h>
+#include <terminal_session/connection.h>
+#include <base/signal.h>
 
 namespace Tcp
 {
@@ -14,12 +16,16 @@ namespace Tcp
 class Tcp::Connection : public Genode::Thread
 {
     private:
+        Terminal::Connection _terminal;
+        Genode::Signal_handler<Connection> _read_sigh;
         int _socket;
         bool _closed;
 
-        void lc_read();
-        void lc_write(void *, Genode::size_t);
+        void lc_read(void *, Genode::size_t, long *);
+        void lc_write(void const *, Genode::size_t, long *);
+        void lc_close();
 
+        void handle_response();
         void entry() override;
 
     public:
