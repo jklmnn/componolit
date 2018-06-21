6c012726362028c2f3be9709c36e29fd34c6443d
Johannes Kliemann <kliemann@componolit.com>
2018-06-21 10:25:59 +0200

    serve valid http response

--- src/test/tcp_echo_server/connection.cc
+++ src/test/tcp_echo_server/connection.cc
@@ -3,6 +3,8 @@
 
 #include <unistd.h>
 
+static char const response[] = "HTTP/1.0 200 OK\nServer: tcpecho\nContent-Type: text/html\nContent-Length: 9\nConnection: Keep-Alive\n\n<html/>\n";
+
 Tcp::Connection::Connection(Genode::Env &env, int socket, char const *label) :
     Genode::Thread(env, label, CONNECTION_BUFFER + 4096),
     _socket(socket),
@@ -22,8 +24,8 @@ void Tcp::Connection::entry()
         LIBC(read, buffer, sizeof(buffer), &received);
         Genode::log(Genode::Cstring(buffer));
         if(received > 0 && !_closed){
-            while(sent < received && !_closed){
-                LIBC(write, &buffer[sent], received - sent, &sent);
+            while(sent < (int)sizeof(response) && !_closed){
+                LIBC(write, &response[sent], sizeof(response) - sent, &sent);
             }
         }
     }
@@ -37,6 +39,7 @@ bool Tcp::Connection::closed() const
 void Tcp::Connection::lc_read(void *buffer, Genode::size_t size, long *result)
 {
     *result += read(_socket, buffer, size);
+    Genode::log(__func__, " ", *result);
     if(*result < 1){
         LIBC(close);
     }
@@ -45,6 +48,7 @@ void Tcp::Connection::lc_read(void *buffer, Genode::size_t size, long *result)
 void Tcp::Connection::lc_write(void const *buffer, Genode::size_t size, long *result)
 {
     *result += write(_socket, buffer, size);
+    Genode::log(__func__, " ", *result);
     if(*result < 1){
         LIBC(close);
     }
